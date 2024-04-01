9ccd864d41f9e521b93f7ad502a77c41e3f7b0f8
Marouane <procompte97@gmail.com>
2024-04-01 00:52:27 +0000

    [FIX] removes error of UNIX sock, and shutdown

--- srcs/requirements/mariadb/tools/init-mariadb.sh
+++ srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-
+SQL_DATABASE=testDB
+SQL_USER=tester
+SQL_PASSWORD=1236
+SQL_ROOT_PASSWORD=123456
 service mariadb start
+
+sleep 5
+
 mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 mariadb -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb  -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-echo  here mother fucker
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-exec mysqld_safe
-# && mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysqld_safe
+
+# mariadb  -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mariadb-admin -u root -p$SQL_ROOT_PASSWORD shutdown
